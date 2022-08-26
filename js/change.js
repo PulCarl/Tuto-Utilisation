f9d61eb34889708f205194d65ed10244a4ea41cd
ᗰᖇ.ᑕᗩᖇᒪ <93263155+PulCarl@users.noreply.github.com>
2022-08-26 11:37:25 +0200

    Add files via upload

---
+++ js/change.js
@@ -0,0 +1,24 @@
+function myFunction() {
+  document.getElementById("introduction").hidden = false;
+  document.getElementById("installation").hidden = true;
+  document.getElementById("mep").hidden = true;
+  document.getElementById("parametrage").hidden = true;
+}
+function myFunction2() {
+  document.getElementById("introduction").hidden = true;
+  document.getElementById("installation").hidden = false;
+  document.getElementById("mep").hidden = true;
+  document.getElementById("parametrage").hidden = true;
+}
+function myFunction3() {
+  document.getElementById("introduction").hidden = true;
+  document.getElementById("installation").hidden = true;
+  document.getElementById("mep").hidden = false;
+  document.getElementById("parametrage").hidden = true;
+}
+function myFunction4() {
+  document.getElementById("introduction").hidden = true;
+  document.getElementById("installation").hidden = true;
+  document.getElementById("mep").hidden = true;
+  document.getElementById("parametrage").hidden = false;
+}
